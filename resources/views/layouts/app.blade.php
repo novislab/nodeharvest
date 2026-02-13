ebd1c757e2395d83b540cad67dca98ea3947623d
novislab <novislab.dev@gmail.com>
2026-02-13 01:51:25 +0100

    Update main layout and configuration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,17 +2,27 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
         <title>{{ $title ?? config('app.name') }}</title>
 
+        @include('partials.favicon')
+        @include('partials.metadata')
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        @livewireStyles
+        @fluxAppearance
     </head>
-    <body>
-        {{ $slot }}
+    <body class="min-h-screen bg-white dark:bg-zinc-950 antialiased">
+        @include('partials.sidebar')
+        @include('partials.header')
 
-        @livewireScripts
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+
+        @include('partials.toast')
+
+        @fluxScripts
     </body>
 </html>
