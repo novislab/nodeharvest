25179627323c50300e16f38312d24847732fe1eb
novislab <novislab.dev@gmail.com>
2026-02-13 01:51:10 +0100

    Add favicon and metadata partials

---
+++ resources/views/partials/favicon.blade.php
@@ -0,0 +1,7 @@
+{{-- Favicon and touch icons --}}
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}">
+<meta name="theme-color" content="#ffffff">
---
+++ resources/views/partials/metadata.blade.php
@@ -0,0 +1,3 @@
+{{-- Basic metadata for private application --}}
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="robots" content="noindex, nofollow">
