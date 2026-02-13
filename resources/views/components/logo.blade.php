bf00a285a4f25d8161e038c752843ce2d7302f1b
novislab <novislab.dev@gmail.com>
2026-02-13 01:51:14 +0100

    Add logo component with light/dark mode support

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/" wire:navigate.hover class="group flex items-center gap-3 dark:opacity-60 dark:hover:opacity-80 transition-opacity">
+    <img src="{{ asset('assets/logo.webp') }}" alt="NodeHarvest" class="h-7 w-auto invert dark:invert-0">
+</a>
