4322aab756eece1503b9235666da7031dcb19c5b
novislab <novislab.dev@gmail.com>
2026-02-18 01:26:11 +0100

    Make href optional in breadcrumb component

--- resources/views/components/dashboard/page-header.blade.php
+++ resources/views/components/dashboard/page-header.blade.php
@@ -4,13 +4,22 @@
 
 <flux:breadcrumbs>
     @foreach ($breadcrumbs as $crumb)
-        <flux:breadcrumbs.item
-            href="{{ $crumb['href'] ?? '#' }}"
-            wire:navigate
-            separator="slash"
-            :active="$loop->last"
-        >
-            {{ $crumb['label'] }}
-        </flux:breadcrumbs.item>
+        @if (!empty($crumb['href']))
+            <flux:breadcrumbs.item
+                href="{{ $crumb['href'] }}"
+                wire:navigate.hover
+                separator="slash"
+                :active="$loop->last"
+            >
+                {{ $crumb['label'] }}
+            </flux:breadcrumbs.item>
+        @else
+            <flux:breadcrumbs.item
+                separator="slash"
+                :active="$loop->last"
+            >
+                {{ $crumb['label'] }}
+            </flux:breadcrumbs.item>
+        @endif
     @endforeach
 </flux:breadcrumbs>
