e58ba1869e6953aeaa21450c56397dae330b3c88
novislab <novislab.dev@gmail.com>
2026-03-09 04:13:02 +0100

    Fix Instruckt to use Laravel config instead of env vars

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 
-import.meta.env.DEV && import.meta.env.VITE_INSTRUCKT_ENABLED !== 'false' && (async () => {
+window.instrucktEnabled && (async () => {
     const { Instruckt } = await import('instruckt');
     new Instruckt({ endpoint: '/instruckt', adapters: ['livewire'] });
 })();
