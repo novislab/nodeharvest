8864dfc2f95d2dc576ee3f0bf2810a6669da2321
novislab <novislab.dev@gmail.com>
2026-05-12 15:58:40 +0100

    Add Instruckt integration with skill and frontend adapter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import "./bootstrap";
+
+window.instrucktEnabled &&
+    (async () => {
+        const { Instruckt } = await import("instruckt");
+        new Instruckt({ endpoint: "/instruckt", adapters: ["livewire"] });
+    })();
