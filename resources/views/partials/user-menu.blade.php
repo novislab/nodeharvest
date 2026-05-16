ad192e9810e603d7ed677e0376a3d0627fd0d405
novislab <novislab.dev@gmail.com>
2026-05-16 17:43:56 +0100

    Fix user menu profile link and add avatar

--- resources/views/partials/user-menu.blade.php
+++ resources/views/partials/user-menu.blade.php
@@ -1,7 +1,7 @@
 <flux:dropdown position="top" align="start" {{ $attributes->merge(['class' => '']) }}>
-    <flux:sidebar.profile name="{{ auth()->user()->name }}" />
+    <flux:sidebar.profile name="{{ auth()->user()->name }}" avatar="{{ asset('storage/avatar/' . auth()->user()->avatar) }}" />
     <flux:menu>
-        <flux:menu.item href="#" icon="user">Profile</flux:menu.item>
+        <flux:menu.item href="{{ route('settings.profile') }}" icon="user" wire:navigate>Profile</flux:menu.item>
         <flux:menu.separator />
         <form method="POST" action="{{ route('action.logout') }}" class="contents">
             @csrf
